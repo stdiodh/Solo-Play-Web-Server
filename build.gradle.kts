aa8c05521d731ee356095894c20e51136451538c
stdiodh <playlistdh@gmail.com>
2024-10-30 11:22:14 +0900

    Add: Swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 }
 
 kotlin {

b6c18aa2cb8d1089cf94dc74c98d1c3d5d681f3d
stdiodh <playlistdh@gmail.com>
2025-05-16 17:38:27 +0900

    test : 장소에 대한 CRUD 서비스와 지역에 대한 장소들을 반환하는 서비스의 테스트코드를 작성하였습니다.

--- src/test/kotlin/com/example/solo_play_web_server/place/service/PlaceServiceTest.kt
+++ src/test/kotlin/com/example/solo_play_web_server/place/service/PlaceServiceTest.kt
@@ -1,6 +1,7 @@
 package com.example.solo_play_web_server.place.service
 
 import com.example.solo_play_web_server.common.exception.CommonNotFoundException
+import com.example.solo_play_web_server.place.dtos.PlaceRequestDTO
 import com.example.solo_play_web_server.place.entity.Place
 import com.example.solo_play_web_server.place.enums.PlaceCategory
 import com.example.solo_play_web_server.place.enums.Region
@@ -26,6 +27,102 @@ class PlaceServiceTest : StringSpec() {
     }
 
     init{
+        "createPlaces() 메소드로 장소를 생성할 수 있다" {
+            val requestDTO = PlaceRequestDTO (
+                _name = "카페",
+                _region = Region.GANGNAM.toString(),
+                _description = "카페 설명",
+                _placeCategory = PlaceCategory.CAFE,
+                _urls = listOf("url")
+            )
+            val expectedPlace = requestDTO.toEntity()
+
+            coEvery { placeRepository.save(any()) } returns Mono.just(expectedPlace)
+
+            val result = placeService.createPlace(requestDTO)
+
+            StepVerifier.create(result)
+                .expectNextMatches { it.name == "카페" && it.region == Region.GANGNAM }
+                .verifyComplete()
+
+            coVerify(exactly = 1) { placeRepository.save(any()) }
+        }
+
+
+        "updatePlace 는 기존 장소 정보를 갱신한다." {
+            val id = "1"
+            val old = Place(id, "Old", Region.MAPO, "old", 0, PlaceCategory.CAFE, emptyList())
+            val dto = PlaceRequestDTO(
+                _name = "New",
+                _region = Region.GANGNAM.toString(),
+                _description = "new",
+                _placeCategory = PlaceCategory.CAFE,
+                _urls = listOf("url"))
+
+            val updated = old.copy(
+                name = dto.name,
+                region = dto.region,
+                description = dto.description,
+                urls = dto.urls
+            )
+
+            coEvery { placeRepository.findById(id) } returns Mono.just(old)
+            coEvery { placeRepository.save(updated) } returns Mono.just(updated)
+
+            val result = placeService.updatePlace(id, dto)
+
+            StepVerifier.create(result)
+                .expectNextMatches { it.name == "New" && it.region == Region.GANGNAM }
+                .verifyComplete()
+
+            coVerify(exactly = 1) { placeRepository.save(any()) }
+        }
+
+
+        "deletePlace 는 기존 장소 정보를 삭제한다."{
+            val id = "1"
+
+            coEvery { placeRepository.deleteById(id) } returns Mono.empty()
+
+            val result = placeService.deletePlace(id)
+
+            StepVerifier.create(result)
+                .verifyComplete()
+
+            coVerify { placeRepository.deleteById(id) }
+        }
+
+
+        "getPlacesByRegion은 해당 지역의 장소들을 반환한다" {
+            val region = "강남구"
+            val data = listOf(
+                Place("1", "A", Region.GANGNAM, "desc", 1, PlaceCategory.CAFE, listOf())
+            )
+
+            coEvery { placeRepository.findByRegion(region) } returns Flux.fromIterable(data)
+
+            val result = placeService.getPlacesByRegion(region)
+
+            StepVerifier.create(result.collectList())
+                .expectNext(data)
+                .verifyComplete()
+        }
+
+        "getPlacesByName은 해당 이름을 포함하는 장소들을 반환한다" {
+            val name = "카페"
+            val data = listOf(
+                Place("1", "좋은카페", Region.MAPO, "desc", 1, PlaceCategory.CAFE, listOf())
+            )
+
+            coEvery { placeRepository.findByNameContaining(name) } returns Flux.fromIterable(data)
+
+            val result = placeService.getPlacesByName(name)
+
+            StepVerifier.create(result.collectList())
+                .expectNext(data)
+                .verifyComplete()
+        }
+
         "incrementSaved 함수를 사용하면 saved 가 1 증가한다." {
             val placeId = "1"
             val originalPlace = Place (

1585d97c9a0aef3a624f9eca447d17b9556b8721
stdiodh <playlistdh@gmail.com>
2025-09-13 15:45:21 +0900

    add: 장소를 생성하기 위한 데이터 모델이 작성되었습니다.

---
+++ src/main/kotlin/com/example/solo_play_web_server/place/entity/Place.kt
@@ -0,0 +1,35 @@
+package com.example.solo_play_web_server.place.entity
+
+import com.example.solo_play_web_server.common.Dto.BaseDocument
+import com.example.solo_play_web_server.place.enum.MainCategory
+import com.example.solo_play_web_server.place.enums.SubCategory
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+
+@Document("places")
+class Place (
+    @Id
+    val id: String? = null,
+
+    @Field("area")
+    val area: String,
+
+    @Field("address")
+    val address: String,
+
+    @Field("description")
+    val description: String,
+
+    @Field("main_category")
+    val mainCategory: MainCategory,
+
+    @Field("sub_category")
+    val subCategory: SubCategory,
+
+    @Field("level")
+    val level: Int,
+
+    @Field("urls")
+    val urls: List<String> = listOf()
+): BaseDocument()

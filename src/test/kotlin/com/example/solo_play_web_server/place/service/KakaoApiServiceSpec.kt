2ab6a02f54ac7f2468f5bc82c6aed8cc897155b4
stdiodh <playlistdh@gmail.com>
2025-09-23 14:45:24 +0900

    test: 카카오 지도 api를 통해 가져오는 장소들에 서비스 로직을 검증하는 테스트 코드가 작성되었습니다.

--- src/test/kotlin/com/example/solo_play_web_server/place/service/KakaoApiServiceSpec.kt
+++ src/test/kotlin/com/example/solo_play_web_server/place/service/KakaoApiServiceSpec.kt
@@ -1,59 +1,98 @@
 package com.example.solo_play_web_server.place.service
 
-import com.example.solo_play_web_server.place.dto.KakaoApiResponse
-import com.example.solo_play_web_server.place.dto.Meta
-import com.example.solo_play_web_server.place.entity.KakaoPlace
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
-import io.mockk.every
-import io.mockk.mockk
+import io.kotest.matchers.string.shouldContain
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.util.UriBuilder
-import reactor.core.publisher.Mono
-import java.net.URI
-import java.util.function.Function
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.isAccessible
+import reactor.test.StepVerifier
+import java.net.URLEncoder
 
 class KakaoApiServiceSpec : BehaviorSpec() {
-    private val webClient = mockk<WebClient>()
-    private val kakaoApiService = KakaoApiService(webClient)
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var kakaoApiService: KakaoApiService
 
     init {
-        Given("장소 검색을 위한 키워드가 주어졌을 때") {
-            val keyword = "카페"
-            val expectedApiResponse = KakaoApiResponse(
-                documents = listOf(
-                    KakaoPlace("1", "스타벅스", "음식점 > 카페", "서울", "서울", "http://place.map.kakao.com/1")
-                ),
-                meta = Meta(true, 1)
-            )
+        beforeSpec {
+            mockWebServer = MockWebServer()
+            mockWebServer.start()
 
-            val apiKeyField = KakaoApiService::class.memberProperties
-                .find { it.name == "kakaoApiKey" }
-            apiKeyField?.let {
-                it.isAccessible = true
-                (it as kotlin.reflect.KMutableProperty<*>).setter.call(kakaoApiService, "TEST_API_KEY")
+            val webClient = WebClient.builder()
+                .baseUrl(mockWebServer.url("/").toString())
+                .build()
+
+            kakaoApiService = KakaoApiService(webClient).apply {
+                val field = this::class.java.getDeclaredField("kakaoApiKey")
+                field.isAccessible = true
+                field.set(this, "TEST_API_KEY")
+            }
+        }
+
+        afterSpec {
+            mockWebServer.shutdown()
+        }
+
+        Given("장소 검색 요청 시") {
+            val keyword = "연남동 카페"
+            val page = 1
+
+            When("카카오 API가 성공적으로 응답하면") {
+                val successResponseJson = """
+                {
+                  "documents": [{
+                    "id": "1", "place_name": "테스트 카페", "category_name": "음식점 > 카페",
+                    "address_name": "서울", "road_address_name": "서울", "place_url": "http://place.map.kakao.com/1"
+                  }],
+                  "meta": { "is_end": true, "total_count": 1 }
+                }
+                """.trimIndent()
+                mockWebServer.enqueue(
+                    MockResponse().setBody(successResponseJson).addHeader("Content-Type", "application/json")
+                )
+
+                val responseMono = kakaoApiService.searchPlacesByKeyword(keyword, page)
+
+                Then("미리 정의한 가짜 데이터를 반환하고, 올바른 요청을 보내야 한다") {
+                    StepVerifier.create(responseMono)
+                        .assertNext { response ->
+                            response.documents.size shouldBe 1
+                            response.documents[0].placeName shouldBe "테스트 카페"
+                        }
+                        .verifyComplete()
+
+                    val recordedRequest = mockWebServer.takeRequest()
+
+                    val encodedKeyword = URLEncoder.encode(keyword, "UTF-8").replace("+", "%20")
+
+                    recordedRequest.path shouldContain "query=$encodedKeyword"
+                    recordedRequest.path shouldContain "page=$page"
+                    recordedRequest.getHeader("Authorization") shouldBe "KakaoAK TEST_API_KEY"
+                }
             }
 
-            val uriSpec = mockk<WebClient.RequestHeadersUriSpec<*>>()
-            val headersSpec = mockk<WebClient.RequestHeadersSpec<*>>()
-            val responseSpec = mockk<WebClient.ResponseSpec>()
+            When("카카오 API가 400 Bad Request 에러를 반환하면") {
+                val errorBody = "{\"errorType\":\"MissingParameter\",\"message\":\"query parameter required\"}"
+                mockWebServer.enqueue(MockResponse().setResponseCode(400).setBody(errorBody))
 
-            every { webClient.get() } returns uriSpec
-            every { uriSpec.uri(any<Function<UriBuilder, URI>>()) } returns headersSpec
-            every { headersSpec.header(any(), any()) } returns headersSpec
-            every { headersSpec.retrieve() } returns responseSpec
-            every { responseSpec.bodyToMono(KakaoApiResponse::class.java) } returns Mono.just(expectedApiResponse)
+                val responseMono = kakaoApiService.searchPlacesByKeyword(keyword, page)
 
-            When("카카오 API 서비스로 장소를 검색하면") {
-                val actualResponse = kakaoApiService.searchPlacesByKeyword(keyword).block()
+                Then("IllegalArgumentException을 발생시켜야 한다") {
+                    StepVerifier.create(responseMono)
+                        .expectError(IllegalArgumentException::class.java)
+                        .verify()
+                }
+            }
 
-                Then("미리 정의한 가짜 데이터를 반환해야 한다") {
-                    actualResponse shouldNotBe null
-                    actualResponse?.documents?.size shouldBe 1
-                    actualResponse?.documents?.get(0)?.placeName shouldBe "스타벅스"
+            When("카카오 API가 500 Internal Server Error를 반환하면") {
+                mockWebServer.enqueue(MockResponse().setResponseCode(500))
+
+                val responseMono = kakaoApiService.searchPlacesByKeyword(keyword, page)
+
+                Then("RuntimeException을 발생시켜야 한다") {
+                    StepVerifier.create(responseMono)
+                        .expectError(RuntimeException::class.java)
+                        .verify()
                 }
             }
         }

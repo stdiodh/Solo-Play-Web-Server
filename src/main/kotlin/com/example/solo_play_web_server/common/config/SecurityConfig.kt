69a925c87f2d6dfd1908f1086e714820560827cb
stdiodh <playlistdh@gmail.com>
2025-10-09 15:10:14 +0900

    refactor: Security Config에 공개 엔드포인트를 단순화 하고 유저 레벨에 따른 장소 검색 엔드포인트를 추가했습니다.

--- src/main/kotlin/com/example/solo_play_web_server/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/solo_play_web_server/common/config/SecurityConfig.kt
@@ -1,7 +1,9 @@
 package com.example.solo_play_web_server.common.config
 
+import com.example.solo_play_web_server.auth.enum.MemberRole
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -20,26 +22,20 @@ class SecurityConfig {
     @Bean
     fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
-            // CSRF(Cross-Site Request Forgery) 보호 비활성화 (Stateless API에서는 보통 비활성화)
             .csrf { it.disable() }
-            // HTTP 기본 인증 비활성화
             .httpBasic { it.disable() }
-            // 폼 기반 로그인 비활성화
             .formLogin { it.disable() }
-            // 경로별 접근 권한 설정
             .authorizeExchange {
                 it.pathMatchers(
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**",
-                    "/webjars/**"
+                    "/swagger-ui/**", "/v3/api-docs/**", "/webjars/**",
+                    "/api/auth/signup/**", "/api/auth/check-email-duplicate",
+                    "/api/auth/email-verify", "/api/auth/email-confirm",
+                    "/api/auth/login"
                 ).permitAll()
-                    it.pathMatchers(
-                        "/api/auth/signup/**",
-                        "/api/auth/check-email-duplicate",
-                        "/api/auth/email-verify",
-                        "/api/auth/email-confirm",
-                        "/api/auth/login"
-                    ).permitAll()
+
+                    .pathMatchers(HttpMethod.GET, "/api/places/recommendations")
+                    .hasRole(MemberRole.USER.name)
+
                     .anyExchange().authenticated()
             }
             .build()
